028d3f69b35d9e6344a6a362d6c7d0120bf575c2
stjhin <stjhin@gmail.com>
2020-10-27 00:17:49 -0400

    first

---
+++ gatsby/.cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/beers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/index.js"))),
+  "component---src-pages-order-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/order.js"))),
+  "component---src-pages-pizza-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/pizza.js"))),
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/pages/slicemasters.js"))),
+  "component---src-templates-pizza-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/templates/Pizza.js"))),
+  "component---src-templates-slicemaster-js": hot(preferDefault(require("/Users/silvialytjhin/Documents/courseGatsy/master-gatsby-master/starter-files/gatsby/src/templates/Slicemaster.js")))
+}
+
